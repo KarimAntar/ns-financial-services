1f21a09ec2db211000cfddf0a7b46557b472beee
Karim Mamdouh <101858852+KarimMamdoh@users.noreply.github.com>
2025-10-30 20:01:52 +0300

    General update.

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,28 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "NS Financial",
   description: "Simplify Your Finances. Focus on Your Business.",
+  openGraph: {
+    title: "NS Financial",
+    description: "Simplify Your Finances. Focus on Your Business.",
+    url: "https://www.nsfinancialservice.com/",
+    siteName: "NS Financial",
+    images: [
+      {
+        url: "/logo_with_gradient.png",
+        width: 1200,
+        height: 630,
+        alt: "NS Financial Logo",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "NS Financial",
+    description: "Simplify Your Finances. Focus on Your Business.",
+    images: ["/logo_with_gradient.png"],
+  },
 };
 
 export default function RootLayout({
